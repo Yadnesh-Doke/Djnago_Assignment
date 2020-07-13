3fd1d035d29c9f7df38c9d612888ecb0424040bc
unknown <yadneshdoke@gmail.com>
2020-07-14 00:42:09 +0530

    Assignment submitted after changes

--- app/views.py
+++ app/views.py
@@ -29,30 +29,7 @@ email = ""
 update_mails = {}
 
 def action() :
-    # print('action ! -> time : {:.1f}s'.format(time.time()-StartTime))
-    # if selected_country_for_updates != "":
-    #     print("SELECTED COUNTRY: ",selected_country_for_updates)
-    #     print("USER EMAIL: ",email)
-    #     subject = f"Today's updates for the covid-19 cases for {selected_country_for_updates}"
-
-    #     selected_country = Country.objects.get(country_name=selected_country_for_updates)
-    #     confirmed = selected_country.cases.total_confirmed
-    #     recovered = selected_country.cases.total_recovered
-    #     deaths = selected_country.cases.total_deaths
-
-    #     message = f"Total confirmed cases : {confirmed} \n Total Recovered cases : {recovered} \n Total Death cases : {deaths}\n"
-    #     print("MESSAGE")
-    #     print(message)
-    #     # send_mail(subject,
-    #     #             message,
-    #     #             settings.EMAIL_HOST_USER,
-    #     #             [email],
-    #     #             fail_silently = False)
-    #     print("Email sent from THIS")
-
-    # else:
-    #     print("NO COUNTRY SELECTED")
-
+    ''' for sending the email updates to the subscribed users for their selected countries '''
     if len(update_mails) != 0:
         print("length of dict:")
         print(len(update_mails))
@@ -81,6 +58,7 @@ def action() :
 
 
 class setInterval :
+    ''' To periodically call a method (in this case, action()) '''
     def __init__(self,interval,action) :
         self.interval=interval
         self.action=action
@@ -97,18 +75,19 @@ class setInterval :
     def cancel(self) :
         self.stopEvent.set()
 
-# start action every 0.6s
+# start action every 30s
 inter=setInterval(30,action)
-# print('just after setInterval -> time : {:.1f}s'.format(time.time()-StartTime))
 
-#will stop interval in 5s
+#will stop interval in 90s
 t=threading.Timer(90,inter.cancel)
 t.start()
 
 
 @login_required(login_url="/login/")
 def index(request):
+    print("INSIDE INDEX.HTML")
     Cases.objects.all().delete()
+    #fetching worldwide stats through api and storing in the database
     world = requests.get("https://covid-19.dataflowkit.com/v1/world")
     world = world.json()
 
@@ -121,7 +100,6 @@ def index(request):
                                 active_cases = int(world["Active Cases_text"].replace(",",""))
                                 )
 
-    # print(cases)
     cases.save()
 
     countries1 = ["USA","brazil","india","russia","peru","chile","mexico","iran","italy","pakistan","south africa","saudi arabia","turkey","germany"]
@@ -129,6 +107,7 @@ def index(request):
 
     Country.objects.all().delete()
 
+    #fetching statistics for countries and storing in the database table
     for country in countries1:
         country_cases = requests.get("https://covid-19.dataflowkit.com/v1/" + country)
         country_cases = country_cases.json()
@@ -147,18 +126,13 @@ def index(request):
                           active_cases = int(country_cases["Active Cases_text"].replace(",",""))
         )
         cases_obj.save()
-        if country_cases["Country_text"].upper() == "USA":
-            print("FROM AUTHENTICATION, USA=",country_cases["Country_text"])
         country_obj = Country(country_name=country_cases["Country_text"].capitalize(),cases=cases_obj)
         country_obj.save()
 
     for country in countries2:
         country_cases = requests.get("https://api.covid19api.com/total/country/" + country)
         country_cases = country_cases.json()
-        print("COUNTRY_CASES: ", type(country_cases))
-        print(country_cases)
         country_cases = country_cases[-2]
-        print(country_cases)
         cases_obj = Cases(
                           total_confirmed = int(country_cases["Confirmed"]),
                           total_recovered = int(country_cases["Recovered"]),
@@ -173,7 +147,7 @@ def index(request):
         country_obj.save()
 
 
-
+    #setting the context dictionary so that data can be used in templates
     world = Cases.objects.all()[0]
     fatality_rate = ((world.total_deaths) / (world.total_confirmed)) * 100
     recovery_rate = ((world.total_recovered) / (world.total_confirmed)) * 100
@@ -190,33 +164,19 @@ def index(request):
         context["country"+str(i)+"_code"] = "/static/assets/img/flags/" + country_codes[country.country_name.lower()]+".png"
         i += 1
 
+    print("LEAVING INDEX.HTML")
     return render(request, "index.html",context)
 
 @login_required(login_url="/login/")
 def pages(request):
     context = {}
     countries = Country.objects.all()
-    # countries = list(Country.objects.all().values()[:8])
-    # countries_json = dumps(countries, cls=DjangoJSONEncoder)
-    # print("COUNTRIES JSON")
-    # print(countries_json)
-
-    # countries_list = []
     countries_dict = {}
     for country in countries:
-        # context[country.country_name.capitalize()] = country
-        # cases = country.cases
-        # countries_dict[(country.country_name.capitalize())] = (cases.__str__())
         countries_dict[(country.country_name.capitalize())] = (country.__str__())
-        # country.cases = country.cases.__str__()
-        # print(country)
-        # countries_list.append(country)
 
-    print("COUNTRIES DICT")
-    print(countries_dict)
+    #setting up the context so that relevant data of countries can be used in template files for rendering
     context["countries"] = countries_dict
-    # context["countries"] = countries_list
-    # print(context)
 
     # All resource paths end in .html.
     # Pick out the html file name from the url. And load that template.
@@ -238,6 +198,8 @@ def pages(request):
 
 @login_required(login_url="/login/")
 def subscribe(request):
+    ''' for storing each user's suscription for their chosen countries for stat updates '''
+    
     global selected_country_for_updates
     global email
     global update_mails
@@ -256,9 +218,6 @@ def subscribe(request):
             subscribed_countries.append(country.capitalize())
 
         update_mails[email_id] = subscribed_countries
-
-        # if country.upper() == "UNITED KINGDOM":
-        #     selected_country_for_updates = "Uk"
         
         message = f"You have subscribed for updates for {country} country."
         send_mail("Updates from Covid-Tracker",
--- authentication/views.py
+++ authentication/views.py
@@ -18,66 +18,7 @@ from .forms import LoginForm, SignUpForm
 from Covid_cases.models import Cases,Country
 
 def login_view(request):
-    # Cases.objects.all().delete()
-    # world = requests.get("https://covid-19.dataflowkit.com/v1/world")
-    # world = world.json()
-
-    # cases = Cases.objects.create(
-    #                             total_confirmed = int(world["Total Cases_text"].replace(",","")),
-    #                             total_recovered = int(world["Total Recovered_text"].replace(",","")),
-    #                             total_deaths = int(world["Total Deaths_text"].replace(",","")),
-    #                             newly_confirmed = int(world["New Cases_text"].replace(",","")),
-    #                             newly_deaths = int(world["New Deaths_text"].replace(",","")),
-    #                             active_cases = int(world["Active Cases_text"].replace(",",""))
-    #                             )
-
-    # # print(cases)
-    # cases.save()
-
-    # countries1 = ["USA","brazil","russia","india","mexico","pakistan"]
-    # countries2 = ["UK","spain","peru","chile","italy","iran","turkey","saudi arabia","germany"]
-    # # countries3 = ["peru","chile","italy","iran","turkey","saudi arabia","germany"]
-
-    # Country.objects.all().delete()
-
-    # for country in countries1:
-    #     country_cases = requests.get("https://covid-19.dataflowkit.com/v1/" + country)
-    #     country_cases = country_cases.json()
-    #     if (country_cases["New Cases_text"].replace(",","")) == "":
-    #         country_cases["New Cases_text"] = "0"
-
-    #     if (country_cases["New Deaths_text"].replace(",","")) == "":
-    #         country_cases["New Deaths_text"] = "0"
-
-    #     cases_obj = Cases(
-    #                       total_confirmed = int(country_cases["Total Cases_text"].replace(",","")),
-    #                       total_recovered = int(country_cases["Total Recovered_text"].replace(",","")),
-    #                       total_deaths = int(country_cases["Total Deaths_text"].replace(",","")),
-    #                       newly_confirmed = int(country_cases["New Cases_text"].replace(",","")),
-    #                       newly_deaths = int(country_cases["New Deaths_text"].replace(",","")),
-    #                       active_cases = int(country_cases["Active Cases_text"].replace(",",""))
-    #     )
-    #     cases_obj.save()
-    #     if country_cases["Country_text"].upper() == "USA":
-    #         print("FROM AUTHENTICATION, USA=",country_cases["Country_text"])
-    #     country_obj = Country(country_name=country_cases["Country_text"].capitalize(),cases=cases_obj)
-    #     country_obj.save()
-
-    # for country in countries2:
-    #     country_cases = requests.get("https://api.covid19api.com/total/country/" + country)
-    #     country_cases = country_cases.json()[-1]
-    #     cases_obj = Cases(
-    #                       total_confirmed = int(country_cases["Confirmed"]),
-    #                       total_recovered = int(country_cases["Recovered"]),
-    #                       total_deaths = int(country_cases["Deaths"]),
-    #                       newly_confirmed = 0,
-    #                       newly_deaths = 0,
-    #                       active_cases = int(country_cases["Active"])
-    #     )
-    #     cases_obj.save()
-
-    #     country_obj = Country(country_name=country_cases["Country"].capitalize(),cases=cases_obj)
-    #     country_obj.save()
+    ''' handling the login to the application '''
 
     form = LoginForm(request.POST or None)
 
@@ -100,6 +41,7 @@ def login_view(request):
     return render(request, "accounts/login.html", {"form": form, "msg" : msg})
 
 def register_user(request):
+    ''' registering the user to the application '''
 
     msg     = None
     success = False
